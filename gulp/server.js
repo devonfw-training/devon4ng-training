99aabc40996e9a649da4e1d9a3ebe8951c43762c
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2015-04-01 18:01:06 +0200

    empty template with hello world

---
+++ gulp/server.js
@@ -0,0 +1,35 @@
+/*global config*/
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var _ = require('lodash');
+var gulpsync = require('gulp-sync')(gulp);
+
+function browserSyncInit(baseDir, files, browser) {
+    browser = browser === undefined ? 'default' : browser;
+    browserSync.instance = browserSync.init(files, {
+        startPath: '/',
+        port: 9000,
+        server: {
+            baseDir: baseDir,
+            routes: null
+        },
+        browser: browser
+    });
+}
+
+gulp.task('serve', gulpsync.sync(['build', 'watch']), function () {
+    browserSyncInit([
+        config.app.tmp(),
+        config.app.src(),
+        'app/bower_components/bootstrap/dist'
+    ], _.flatten([
+        config.css.dest.path(),
+        config.js.src()
+    ]));
+});
+
+gulp.task('serve:dist', ['build:dist'], function () {
+    browserSyncInit('dist');
+});
