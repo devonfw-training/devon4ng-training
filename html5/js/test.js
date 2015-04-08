59fa2923423e3652dfcef021626de0eb4d0f8a9e
Marek Matczak <marek.matczak@capgemini.com>
2015-04-08 07:44:41 +0200

    ide and tools added; minor changes

--- html5/js/test.js
+++ html5/js/test.js
@@ -3,8 +3,9 @@ var webStorageSupported = typeof (Storage) !== 'undefined';
 if (webStorageSupported) {
     localStorage.setItem('myName', 'John Example');
     sessionStorage.setItem('myName', 'John Example')
-    console.log('From local:' + localStorage.getItem('myName'));
-    console.log('From session:' + sessionStorage.getItem('myName'));
+    
+    console.log('From local: ' + localStorage.getItem('myName'));
+    console.log('From session: ' + sessionStorage.getItem('myName'));
 } else {
     console.log('Web Storage not supported!');
 }
