9613b2f727b99cfe6cad0cf4d0306b399fb8b1b0
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2015-11-10 15:51:44 +0100

    new app template

--- gulp/server.js
+++ gulp/server.js
@@ -3,6 +3,7 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var middleware = require('./proxy');
 var _ = require('lodash');
 var gulpsync = require('gulp-sync')(gulp);
 
@@ -13,21 +14,24 @@ function browserSyncInit(baseDir, files, browser) {
         port: 9000,
         server: {
             baseDir: baseDir,
-            routes: null
+            middleware: middleware,
+            routes: {
+                '/bower_components': 'bower_components'
+            }
         },
-        browser: browser
+        browser: browser,
+        ghostMode: false
     });
 }
 
 gulp.task('serve', gulpsync.sync(['build', 'watch']), function () {
     browserSyncInit([
-        config.app.tmp(),
-        config.app.src(),
-        'app/bower_components/bootstrap/dist'
+        config.paths.tmp,
+        config.paths.src,
+        'bower_components/bootstrap/dist'
     ], _.flatten([
-        config.css.dest.path(),
-        config.js.src(),
-        '.tmp/index.html'
+            config.paths.tmp + '/**',
+            config.paths.src + '/**/*.js'
     ]));
 });
 
