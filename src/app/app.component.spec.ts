37f520ab1ce99d598d80008ed19af94926ab87b4
pschmoek <philip.schmoekel@googlemail.com>
2018-05-15 12:16:36 +0200

    UnitTest fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
+import { MovieOverviewComponent } from './movies/movie-overview/movie-overview.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent, MovieDetailsComponent
+        AppComponent, MovieDetailsComponent, MovieOverviewComponent
       ],
     }).compileComponents();
   }));
