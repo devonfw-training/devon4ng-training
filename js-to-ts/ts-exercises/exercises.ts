c971010123b37ca6f26009d95dd473f1de69cc14
pschmoek <philip.schmoekel@googlemail.com>
2018-10-19 18:29:48 +0200

    implement new unit test for typescript session

---
+++ js-to-ts/ts-exercises/exercises.ts
@@ -0,0 +1,23 @@
+function reverseFunc(val: number): number {
+  // TODO
+  
+  return val;
+}
+
+function findLongestWordFunc(sentence: string): string {
+  // TODO
+
+  return sentence;
+}
+
+function getElementsGreater(numbers: number[], threshold: number): number[] {
+  // TODO
+
+  return numbers;
+}
+
+function parseAndBuild(json: string): {[key: string]: {count: number, allNew: boolean}} {
+  // TODO
+
+  return {};
+}
