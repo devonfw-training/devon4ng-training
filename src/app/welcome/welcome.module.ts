fc492ba639972811ae32ab5aee939f3ef2fd8ff0
Capgemini\dglagla <dorian.glagla@capgemini.com>
2019-06-04 17:10:30 +0200

    FormsModuleImport

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class WelcomeModule { }
