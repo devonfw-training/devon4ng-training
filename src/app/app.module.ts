57b5731a26d26acac18a5374f0e06b46327114b3
Philipp Kief <philipp.kief@gmx.de>
2023-01-10 15:42:36 +0000

    Update to Angular 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import {RouterModule, Routes} from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
 
-
-import { AppComponent } from './app.component';
-import { MoviesModule } from './movies/movies.module';
-import { MovieOverviewComponent } from './movies/movie-overview/movie-overview.component';
-import { PageNotFoundComponent } from './static-pages/page-not-found/page-not-found.component';
-import { StaticPagesModule } from './static-pages/static-pages.module';
-import { LoginModule } from './login/login.module';
-import { RegisterComponent } from './login/register/register.component';
-import { AboutComponent } from './static-pages/about/about.component';
+import { AppComponent } from "./app.component";
+import { MoviesModule } from "./movies/movies.module";
+import { MovieOverviewComponent } from "./movies/movie-overview/movie-overview.component";
+import { PageNotFoundComponent } from "./static-pages/page-not-found/page-not-found.component";
+import { StaticPagesModule } from "./static-pages/static-pages.module";
+import { LoginModule } from "./login/login.module";
+import { RegisterComponent } from "./login/register/register.component";
+import { AboutComponent } from "./static-pages/about/about.component";
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'movies', component: MovieOverviewComponent },
-  { path: 'movies/:id', component: MovieOverviewComponent },
-  { path: '**', component: PageNotFoundComponent}
+  { path: "", redirectTo: "/register", pathMatch: "full" },
+  { path: "register", component: RegisterComponent },
+  { path: "about", component: AboutComponent },
+  { path: "movies", component: MovieOverviewComponent },
+  { path: "movies/:id", component: MovieOverviewComponent },
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     MoviesModule,
     HttpClientModule,
     StaticPagesModule,
-    LoginModule
+    LoginModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
