bb9cd80e2563977c95c43f8f5200859f152bee2f
Philipp Kief <philipp.kief@gmx.de>
2023-01-11 09:54:44 +0000

    Add type information to registration form

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,43 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from "@angular/forms";
+
+interface RegistrationFormType {
+  firstName: FormControl<string | null>;
+  lastName: FormControl<string | null>;
+  email: FormControl<string | null>;
+  passwords: FormGroup<PasswordFormType>;
+}
+
+type PasswordFormType = {
+  password: FormControl<string | null>;
+  passwordRepeat: FormControl<string | null>;
+};
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
-
-  registrationForm: FormGroup;
+  registrationForm: FormGroup<RegistrationFormType>;
 
   constructor(private formBuilder: FormBuilder) {
-    this.registrationForm = this.formBuilder.group({
-      firstname: ['', Validators.required],
-      lastname:  ['', Validators.required],
-      email: ['example@mail.com', Validators.email],
+    this.registrationForm = this.formBuilder.group<RegistrationFormType>({
+      firstName: this.formBuilder.control("", Validators.required),
+      lastName: this.formBuilder.control("", Validators.required),
+      email: this.formBuilder.control("example@mail.com", Validators.email),
       passwords: this.formBuilder.group(
         {
-          password:  ['', Validators.minLength(8)],
-          passwordRepeat: ''
-      }, {
-        validators: this.validateEqual
-      })
+          password: this.formBuilder.control("", [
+            Validators.required,
+            Validators.minLength(8),
+          ]),
+          passwordRepeat: this.formBuilder.control(""),
+        },
+        {
+          validators: this.validateEqual,
+        }
+      ),
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   register() {
     console.log(this.registrationForm.value);
   }
 
-
-  validateEqual(pws: FormGroup): ValidationErrors|null {
+  validateEqual(pws: FormGroup<PasswordFormType>): ValidationErrors | null {
     if (pws.controls.password.value !== pws.controls.passwordRepeat.value) {
       return { notequal: true };
     }
     return null;
   }
-
 }
