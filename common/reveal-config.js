755912aadce9b79e3fe086bf2b5fcfac5c1ad00d
Alexander Dorn <alexander.dorn@capgemini.com>
2024-12-09 10:21:02 +0100

    Update reveal js and already migrate two topics and one agenda manually

--- common/reveal-config.js
+++ common/reveal-config.js
@@ -1,6 +1,7 @@
 // Full list of configuration options available at:
-// https://github.com/hakimel/reveal.js#configuration
-var revealConfig = {
+// https://revealjs.com/config/
+const revealConfig = {
+  hash: true,
   controls: true,
   progress: true,
   history: true,
@@ -8,50 +9,6 @@ var revealConfig = {
   minScale: 1,
   maxScale: 1,
   transition: "fade", // none/fade/slide/convex/concave/zoom
-
-  // Optional reveal.js plugins
-  dependencies: [
-    {
-      src: "../node_modules/reveal.js/lib/js/classList.js",
-      condition: function () {
-        return !document.body.classList;
-      },
-    },
-    {
-      src: "../node_modules/reveal.js/plugin/markdown/marked.js",
-      condition: function () {
-        return !!document.querySelector("[data-markdown]");
-      },
-    },
-    {
-      src: "../node_modules/reveal.js/plugin/markdown/markdown.js",
-      condition: function () {
-        return !!document.querySelector("[data-markdown]");
-      },
-    },
-    {
-      src: "../node_modules/reveal.js/plugin/highlight/highlight.js",
-      async: true,
-      condition: function () {
-        return !!document.querySelector("pre code");
-      },
-      callback: function () {
-        hljs.initHighlightingOnLoad();
-      },
-    },
-    {
-      src: "../node_modules/reveal.js/plugin/zoom-js/zoom.js",
-      async: true,
-    },
-    {
-      src: "../node_modules/reveal.js/plugin/notes/notes.js",
-      async: true,
-    },
-    {
-      src: "../node_modules/reveal.js/plugin/menu/menu.js",
-      async: true,
-    },
-  ],
   menu: {
     hideMissingTitles: true,
     custom: true,
